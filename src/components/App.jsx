fafbce3a897265a1689e7e2e03d5dbc243a4eeaa
Artem Makhov <makhovartem23@gmail.com>
2022-08-14 13:47:29 +0300

    add input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 // import { Box } from './Box';
 import { GlobalStyle } from './GlobalStyle';
 import { Box } from './Box';
-import TodoList from './TodoList';
+// import TodoList from './TodoList';
+import Form from './Form/Form';
 import { Component } from 'react';
 import initialTodos from './todos.json';
 
 class App extends Component {
   state = {
     todos: initialTodos,
-    name: '',
-    tag:'',
+   
   };
 
   deleteTodo = (todoId) => {
@@ -18,41 +18,31 @@ class App extends Component {
     }))
   }
 
-  hendleChange = event => {
-    const { name, value } = event.currentTarget;
-
-    this.setState({
-      [name]: value,
-    })
+  formSubmitHendler = data => {
+    console.log(data);
   }
+
   
   render() {
-    const { todos } = this.state;
+    // const { todos } = this.state;
 
-    const totalTodoCount = todos.length;
-    const completedTodosCount = todos.reduce(
-      (total, todo) => (todo.completed ? total + 1 : total),
-      0,
-    );
+    // const totalTodoCount = todos.length;
+    // const completedTodosCount = todos.reduce(
+    //   (total, todo) => (todo.completed ? total + 1 : total),
+    //   0,
+    // );
 
    
     
     return (
 <>
         <Box>
-          <form>
-            <label>
-              Name <input type="text" name='name' value={this.state.name} onChange={this.hendleChange} />
-            </label>
-            <label>
-              Tag <input type="text" name='tag' value={this.state.tag} onChange={this.hendleChange} />
-            </label>
-          </form>
-          <div>
+          <Form onSubmit={this.formSubmitHendler} />     
+          {/* <div>
             <p>Общее кол-во:{ totalTodoCount}</p>
             <p>Кол-во выполненых:{completedTodosCount}</p>
           </div>
-      <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />  
+      <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />   */}
   </Box>
   <GlobalStyle/>
 </>
